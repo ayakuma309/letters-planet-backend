bcb3da43791bd694379b13cbbb2ed05a4f4a4406
ayakuma309 <ayako.30901@gmail.com>
2023-10-18 21:06:30 +0900

    fix: posts修正

--- routers/posts.js
+++ routers/posts.js
@@ -7,9 +7,9 @@ const prisma = new PrismaClient();
 
 //投稿API
 router.post("/post", isAuthenticated, async (req, res) => {
-  const { videoId, url, title, description, tags } = req.body;
+  const { videoId, url, title, releaseAt, tags } = req.body;
 
-  if (!title || !url || !description) {
+  if (!title || !url || !releaseAt) {
     return res.status(400).json({ error: "All fields are required" });
   }
   try {
@@ -32,19 +32,13 @@ router.post("/post", isAuthenticated, async (req, res) => {
         videoId,
         url,
         title,
-        description,
-        authorId: req.userId,
+        releaseAt,
         tags: {
           connect: createdTags.map((tag) => ({ id: tag.id })),
         },
       },
       //usernameアクセスするためにincludeを使う
       include: {
-        author: {
-          include: {
-            profile: true,
-          }
-        },
         tags: true,
       },
     });
@@ -58,44 +52,17 @@ router.post("/post", isAuthenticated, async (req, res) => {
 router.get("/get_latest_posts", async (req, res) => {
   try {
     const latestPosts = await prisma.post.findMany({
-      // take: 60,
       orderBy: {
-        createdAt: "desc",
+        releaseAt: "desc",
       },
        //usernameアクセスするためにincludeを使う
       include: {
-        author: {
-          include: {
-            profile: true,
-          }
-        },
         tags: true,
       },
     });
     return res.status(200).json(latestPosts);
   } catch (err) {
-    return res.status(500).json({ err: "something went wrong" });
-  }
-});
-
-//その閲覧しているユーザーの投稿内容だけを取得
-router.get("user/:userId", async(req,res) => {
-  const user = req.params;
-  try {
-    const userPosts = await prisma.post.findMany({
-      where: {
-        authorId: Number(user.userId),
-      },
-      orderBy: {
-        createdAt: "desc",
-      },
-      include: {
-        author: true,
-        tags: true,
-      },
-    });
-    return res.status(200).json(userPosts);
-  } catch (err) {
+    console.log(err);
     return res.status(500).json({ err: "something went wrong" });
   }
 });
@@ -109,7 +76,6 @@ router.get("/post/:id", async (req, res) => {
         id: Number(id)
       },
       include: {
-        author: true,
         tags: true,
       },
     });
@@ -142,12 +108,6 @@ router.delete("/post/:id", isAuthenticated, async (req, res) => {
       return res
         .status(404)
         .json({ message: "投稿が見つかりませんでした。" });
-    }
-
-    if (post.authorId !== req.userId) {
-      return res
-        .status(401)
-        .json({ message: "投稿の削除権限がありません。" });
     }
 
     await prisma.post.delete({
